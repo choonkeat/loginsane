9df9aa11b63deae563120de2a7a67ad42d2f2ac8
choonkeat <choonkeat@gmail.com>
2010-01-29 14:32:17 +0800

    accept notice/error message from params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,7 @@ module ApplicationHelper
     "Hi #{h(session[:profile_name])} | #{button_to("Logout", logout_sites_path)}" +
     "<h5 class='bread'>#{args.collect {|(txt,link)| link_to_unless_current(h(txt), link) }.join(' &raquo; ')}</h5>"
   end
+  def display_flash(type, content)
+    "<p class=\"#{h(type)}\">#{h(content)}</p>" if not content.blank?
+  end
 end
